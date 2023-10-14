8ca3ece6274846a9e9551cfbbaa8c8c340774b43
zxrow <49530313+zxrow@users.noreply.github.com>
2023-10-14 07:08:30 +0700

    Create agung.js

---
+++ agung.js
@@ -0,0 +1,237 @@
+require('./agungsettings')
+const { WA_DEFAULT_EPHEMERAL, getAggregateVotesInPollMessage, generateWAMessageFromContent, proto, generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, downloadContentFromMessage, areJidsSameUser, getContentType } = require("@whiskeysockets/baileys")
+const fs = require("fs");
+const chalk = require("chalk");
+const crypto = require("crypto");
+const { exec, spawn, execSync } = require("child_process");
+const axios = require("axios");
+const fetch = require("node-fetch");
+const Jimp = require("jimp");
+const util = require("util");
+const { sizeFormatter} = require("human-readable")
+const format = sizeFormatter()
+const cheerio = require('cheerio')
+const yts = require('yt-search')
+const fsx = require('fs-extra')
+const hxz = require('hxz-api')
+const ytdl = require('ytdl-core')
+const moment = require('moment-timezone')
+const hariini = moment.tz('Asia/Jakarta').format('dddd, DD MMMM YYYY')
+const hariiini = moment.tz('Asia/Jakarta').format('DD MMMM YYYY')
+const { JSDOM } = require('jsdom')
+const { color, bgcolor, mycolor } = require('./lib/color')
+const { imageToWebp, videoToWebp, writeExifImg, writeExifVid } = require('./lib/exif')
+const { UploadFileUgu, webp2mp4File, TelegraPh } = require("./lib/uploader")
+const { Primbon } = require("scrape-primbon")
+const { TelegraPH } = require("./lib/TelegraPH")
+const agung2 = require('./scrap/mediafire')
+const { remini } = require('./lib/remini')
+const primbon = new Primbon()
+const { smsg, formatp, tanggal, formatDate, getTime, isUrl, sleep, clockString, runtime, fetchJson, getBuffer, jsonformat, parseMention, getRandom } = require('./lib/functions')
+const addusrp = JSON.parse(fs.readFileSync('./database/user.json'))
+
+module.exports = agung = async (agung, m, chatUpdate, store) => {
+try {
+const body = (m.mtype === 'conversation') ? m.message.conversation : (m.mtype == 'imageMessage') ? m.message.imageMessage.caption : (m.mtype == 'videoMessage') ? m.message.videoMessage.caption : (m.mtype == 'extendedTextMessage') ? m.message.extendedTextMessage.text : (m.mtype == 'buttonsResponseMessage') ? m.message.buttonsResponseMessage.selectedButtonId : (m.mtype == 'listResponseMessage') ? m.message.listResponseMessage.singleSelectReply.selectedRowId : (m.mtype == 'templateButtonReplyMessage') ? m.message.templateButtonReplyMessage.selectedId : (m.mtype === 'messageContextInfo') ? (m.message.buttonsResponseMessage?.selectedButtonId || m.message.listResponseMessage?.singleSelectReply.selectedRowId || m.text) : ''
+const budy = (typeof m.text == 'string' ? m.text : '')
+const prefix = /^[°#*+,.?=''():√%!¢£¥€π¤ΠΦ_&`™©®Δ^βα¦|/\\©^]/.test(body) ? body.match(/^[°#*+,.?=''():√%¢£¥€π¤ΠΦ_&!`™©®Δ^βα¦|/\\©^]/gi) : '.'
+const chath = (m.mtype === 'conversation' && m.message.conversation) ? m.message.conversation : (m.mtype == 'imageMessage') && m.message.imageMessage.caption ? m.message.imageMessage.caption : (m.mtype == 'documentMessage') && m.message.documentMessage.caption ? m.message.documentMessage.caption : (m.mtype == 'videoMessage') && m.message.videoMessage.caption ? m.message.videoMessage.caption : (m.mtype == 'extendedTextMessage') && m.message.extendedTextMessage.text ? m.message.extendedTextMessage.text : (m.mtype == 'buttonsResponseMessage' && m.message.buttonsResponseMessage.selectedButtonId) ? m.message.buttonsResponseMessage.selectedButtonId : (m.mtype == 'templateButtonReplyMessage') && m.message.templateButtonReplyMessage.selectedId ? m.message.templateButtonReplyMessage.selectedId : (m.mtype == "listResponseMessage") ? m.message.listResponseMessage.singleSelectReply.selectedRowId : (m.mtype == "messageContextInfo") ? m.message.listResponseMessage.singleSelectReply.selectedRowId : ''
+const content = JSON.stringify(m.message)
+const { type, quotedMsg, mentioned, now, fromMe } = m
+const isCmd = body.startsWith(prefix)
+const from = m.key.remoteJid
+const command = body.replace(prefix, '').trim().split(/ +/).shift().toLowerCase()
+const args = body.trim().split(/ +/).slice(1)
+const full_args = body.replace(command, '').slice(1).trim()
+const pushname = m.pushName
+const botNumber = await agung.decodeJid(agung.user.id)
+const isCreator = [botNumber, ...global.owner].map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
+const itsMe = m.sender == botNumber ? true : false
+const text = q = args.join(" ")
+const quoted = m.quoted ? m.quoted : m
+const mime = (quoted.msg || quoted).mimetype || ''
+const isMedia = /image|video|sticker|audio/.test(mime)
+const { chats } = m
+
+const tanggal = moment.tz('Asia/Jakarta').format('DD/MM/YY')
+
+const sender = m.isGroup ? (m.key.participant ? m.key.participant : m.participant) : m.key.remoteJid
+const groupMetadata = m.isGroup ? await agung.groupMetadata(m.chat).catch(e => {}) : ''
+const groupName = m.isGroup ? groupMetadata.subject : ''
+const participants = m.isGroup ? await groupMetadata.participants : ''
+const groupAdmins = m.isGroup ? await participants.filter(v => v.admin !== null).map(v => v.id) : ''
+const groupOwner = m.isGroup ? groupMetadata.owner : ''
+const groupMembers = m.isGroup ? groupMetadata.participants : ''
+	const isBotAdmins = m.isGroup ? groupAdmins.includes(botNumber) : false
+const isImage = m.mtype === "imageMessage"
+const isQuotedImage = m.mtype === "extendedTextMessage" && content.includes("imageMessage")
+const isGroupAdmins = m.isGroup ? groupAdmins.includes(m.sender) : false
+	const isAdmins = m.isGroup ? groupAdmins.includes(m.sender) : false
+	
+if (!agung.public) {
+if (!m.key.fromMe) return
+}
+
+if (isCmd && m.isGroup) { console.log(chalk.bold.rgb(255, 178, 102)('\x1b[1;31m~\x1b[1;37m> [\x1b[1;32mCMD\x1b[1;37m]'), chalk.bold.rgb(153, 255, 153)(command), chalk.bold.rgb(204, 204, 0)("from"), chalk.bold.rgb(153, 255, 204)(pushname), chalk.bold.rgb(204, 204, 0)("in"), chalk.bold.rgb(255, 178, 102)("Group Chat"), chalk.bold('[' + args.length + ']')); }
+if (isCmd && !m.isGroup) { console.log(chalk.bold.rgb(255, 178, 102)('\x1b[1;31m~\x1b[1;37m> [\x1b[1;32mCMD\x1b[1;37m]'), chalk.bold.rgb(153, 255, 153)(command), chalk.bold.rgb(204, 204, 0)("from"), chalk.bold.rgb(153, 255, 204)(pushname), chalk.bold.rgb(204, 204, 0)("in"), chalk.bold.rgb(255, 178, 102)("Private Chat"), chalk.bold('[' + args.length + ']')); }
+		
+try {
+ppuser = await agung.profilePictureUrl(m.sender, 'image')
+} catch (err) {
+ppuser = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60'
+}
+ppnyauser = await getBuffer(ppuser)
+
+const generateProfilePicture = async(buffer) => {
+const jimp_1 = await Jimp.read(buffer);
+const resz = jimp_1.getWidth() > jimp_1.getHeight() ? jimp_1.resize(550, Jimp.AUTO) : jimp_1.resize(Jimp.AUTO, 650)
+const jimp_2 = await Jimp.read(await resz.getBufferAsync(Jimp.MIME_JPEG));
+return {
+img: await resz.getBufferAsync(Jimp.MIME_JPEG)
+}
+}
+const tokenn = await axios.get("https://downvideo.quora-wiki.com/tiktok-video-downloader#url=" + url);
+let a = cheerio.load(tokenn.data);
+let token = a("#token").attr("value");
+const param = {
+    url: url,
+    token: token,
+};
+const { data } = await axios.request("https://downvideo.quora-wiki.com/system/action.php", {
+method: "post",
+data: new URLSearchParams(Object.entries(param)),
+headers: {
+    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
+    "referer": "https://downvideo.quora-wiki.com/tiktok-video-downloader",
+},
+    }
+);
+return {
+    status: 200,
+    title: data.title,
+    thumbnail: data.thumbnail,
+    duration: data.duration,
+    media: data.medias,
+};
+    } catch (e) {
+return e
+    }
+}
+//━━━━━━━━━━━━━━━[ FAKE FAKE ]━━━━━━━━━━━━━━━━━//
+const fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': 'agungDev', 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;agungDev;;;\nFN:agungev\nitem1.TEL;waid=6289655478810:6289655478810\nitem1.X-ABLabel:Ponsel\nEND:VCARD`, 'jpegThumbnail': thumb, thumbnail: thumb,sendEphemeral: true}}}
+const reply = (teks) => {
+agung.sendMessage(m.chat, { text: teks ,
+contextInfo:{
+forwardingScore: 9999999, 
+isForwarded: true
+}
+}, { quoted : repPy })
+}
+//HAYO MAU NGAPAIN//
+const repPy = {
+	key: {
+		remoteJid: '0@s.whatsapp.net',
+		fromMe: false,
+		id: `${global.namabot}`,
+		participant: '0@s.whatsapp.net'
+	},
+	message: {
+		requestPaymentMessage: {
+			currencyCodeIso4217: "USD",
+			amount1000: 999999999,
+			requestFrom: '0@s.whatsapp.net',
+			noteMessage: {
+				extendedTextMessage: {
+					text: `${global.namabot}`
+				}
+			},
+			expiryTimestamp: 999999999,
+			amount: {
+				value: 91929291929,
+				offset: 1000,
+				currencyCode: "USD"
+			}
+		}
+	}
+}
+//=================================================//
+//━━━━━━━━━━━━━━━[ DIWNLOAD YTMP3 ]━━━━━━━━━━━━━━━━━//
+const downloadMp4 = async (Link) => {
+try {
+await ytdl.getInfo(Link)
+let mp4File = getRandom('.mp4')
+console.log(color('Download Video With ytdl-core'))
+let nana = ytdl(Link)
+.pipe(fs.createWriteStream(mp4File))
+.on('finish', async () => {
+await agung.sendMessage(m.chat, { video: fs.readFileSync(mp4File), caption: mess.succes, gifPlayback: false }, { quoted: m })
+fs.unlinkSync(`./${mp4File}`)
+})
+} catch (err) {
+m.reply(`${err}`)
+}
+}
+
+
+//RPGGAMES
+
+//=================================================//
+//=================================================//
+
+const downloadMp3 = async (Link) => {
+try {
+await ytdl.getInfo(Link)
+let mp3File = getRandom('.mp3')
+console.log(color('Download Audio With ytdl-core'))
+ytdl(Link, { filter: 'audioonly' })
+.pipe(fs.createWriteStream(mp3File))
+.on('finish', async () => {
+await agung.sendMessage(m.chat, { audio: fs.readFileSync(mp3File), mimetype: 'audio/mp4' }, { quoted: m })
+fs.unlinkSync(mp3File)
+})
+} catch (err) {
+m.reply(`${err}`)
+}
+}
+async function sendagungMessage(chatId, message, options = {}){
+    let generate = await generateWAMessage(chatId, message, options)
+    let type2 = getContentType(generate.message)
+    if ('contextInfo' in options) generate.message[type2].contextInfo = options?.contextInfo
+    if ('contextInfo' in message) generate.message[type2].contextInfo = message?.contextInfo
+    return await agung.relayMessage(chatId, generate.message, { messageId: generate.key.id })
+}
+//━━━━━━━━━━━━━━━[ BATAS FAKE ]━━━━━━━━━━━━━━━━━//
+const createSerial = (size) => {
+return crypto.randomBytes(size).toString('hex').slice(0, size)
+}
+
+function randomNomor(min, max = null) {
+if (max !== null) {
+min = Math.ceil(min);
+max = Math.floor(max);
+return Math.floor(Math.random() * (max - min + 1)) + min;
+} else {
+return Math.floor(Math.random() * min) + 1
+}
+}
+function monospace(string) {
+return '```' + string + '```'
+}
+
+const pickRandom = (arr) => {
+return arr[Math.floor(Math.random() * arr.length)]
+}
+
+global.addUserPanel = (email, username, expired, _db) => {
+var obj_add = {
+email: email,
+username: username,
+expired: expired
+}
+_db.push(obj_add)
+fs.writeFileSync('./database/user.json', JSON.stringify(_db, null, 3))
+}
+
+switch (command) {
+//━━━━━━━━━━━━━━━[ MENU ]━━━━━━━━━━━━━━━━━//
